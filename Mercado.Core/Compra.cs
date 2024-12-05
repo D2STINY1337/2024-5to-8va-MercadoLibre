0cf8a1407071f10d095389f12e7c70f4553d0273
Bruneizer <carlosbruno3856@gmail.com>
2024-12-05 10:30:15 -0300

    Arreglos

--- Mercado.Core/Compra.cs
+++ Mercado.Core/Compra.cs
@@ -2,22 +2,17 @@ namespace Mercado.Core;
 
 public class Compra
 {
-    public class Compra
-    {
-        public int IdComprador { get; set; }
-        public int IdProducto { get; set; }
-        public DateTime FechaHora { get; set; }
-        public ushort Cantidad { get; set; }
-        public decimal Precio { get; set; }
+    public int IdComprador { get; set; }
+    public int IdProducto { get; set; }
+    public DateTime FechaHora { get; set; }
+    public ushort Cantidad { get; set; }
+    public decimal Precio { get; set; }
 
-        public Usuario Comprador { get; set; }
-        public Producto Producto { get; set; }
+    public Usuario Comprador { get; set; }
+    public Producto Producto { get; set; }
 
-        public override string ToString()
-        {
-            return $"Comprador ID: {IdComprador}, Producto ID: {IdProducto}, Fecha: {FechaHora}, Cantidad: {Cantidad}, Precio Total: {Cantidad * Precio}";
-        }
-    }
+    public override string ToString()
+        => $"Comprador ID: {IdComprador}, Producto ID: {IdProducto}, Fecha: {FechaHora}, Cantidad: {Cantidad}, Precio Total: {Cantidad * Precio}";
 }
 
 
--- Mercado.Core/Usuario.cs
+++ Mercado.Core/Usuario.cs
@@ -2,19 +2,13 @@
 
 public class Usuario
 {
-   public class Usuario
-    {
-        public int IdUsuario { get; set; }
-        public string Nombre { get; set; }
-        public string Apellido { get; set; }
-        public int Telefono { get; set; }
-        public string Email { get; set; }
-        public string Pass { get; set; }
-
-        public override string ToString()
-        {
-            return $"ID: {IdUsuario}, Nombre: {Nombre} {Apellido}, Email: {Email}, Teléfono: {Telefono}";
-        }
-    }
+    public int IdUsuario { get; set; }
+    public string Nombre { get; set; }
+    public string Apellido { get; set; }
+    public int Telefono { get; set; }
+    public string Email { get; set; }
+    public string Pass { get; set; }
+    public override string ToString()
+        => $"ID: {IdUsuario}, Nombre: {Nombre} {Apellido}, Email: {Email}, Teléfono: {Telefono}";
 }
 
